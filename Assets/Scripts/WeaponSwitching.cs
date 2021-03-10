991a8f53803409ce1df4774e2745359bfc27e4ba
gmahoney323 <grant.mahoney@mymail.champlain.edu>
2021-03-09 20:34:18 -0500

    added multiple weapons and weapon switching

---
+++ Assets/Scripts/WeaponSwitching.cs
@@ -0,0 +1,52 @@
+﻿//credit to brackeys on youtube: 
+//https://www.youtube.com/watch?v=Dn_BUIVdAPg&list=PLPV2KyIb3jR7dFbE2UQYu7QWMdUgDnlnk&index=3
+
+using UnityEngine;
+
+public class WeaponSwitching : MonoBehaviour
+{
+    public int selectedWeapon = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectWeapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int previousSelectedWeapon = selectedWeapon;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (selectedWeapon >= transform.childCount - 1)
+                selectedWeapon = 0;
+            else
+                selectedWeapon++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectedWeapon <= 0)
+                selectedWeapon = transform.childCount-1;
+            else
+                selectedWeapon--;
+        }
+
+        if (previousSelectedWeapon != selectedWeapon)
+            SelectWeapon();
+    }
+
+    void SelectWeapon()
+    {
+        int i = 0;
+        foreach(Transform weapon in transform)
+        {
+            if (i == selectedWeapon)
+                weapon.gameObject.SetActive(true);
+            else
+                weapon.gameObject.SetActive(false);
+            i++;
+        }
+    }
+}
