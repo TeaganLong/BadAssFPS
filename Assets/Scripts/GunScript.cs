94f24ab121b8d27d488d571d57a117f76db9aa19
gmahoney323 <grant.mahoney@mymail.champlain.edu>
2021-03-16 17:38:55 -0400

    added weapon ammo count and reloading with basic animations

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -2,25 +2,69 @@
 //https://www.youtube.com/watch?v=THnivyG0Mvo&list=PLPV2KyIb3jR7dFbE2UQYu7QWMdUgDnlnk&index=2
 
 using UnityEngine;
+using System.Collections;
 
 public class GunScript : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
 
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+
     public Camera cam;
+
+    public Animator animator;
+
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
 
     // Update is called once per frame
     void Update()
     {
+        if (isReloading)
+            return;
+
+        if (currentAmmo<=0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
     }
 
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        animator.SetBool("Reloading", true);
+        yield return new WaitForSeconds(reloadTime - .25f);
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(.25f);
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     void Shoot()
     {
+        currentAmmo--;
+
         RaycastHit hit;
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit))
         {
