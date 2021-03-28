faac585e173d1a13d3322882f0c9196944c7d92b
TeaganLong <teagan.long@mymail.champlain.edu>
2021-03-28 14:56:24 -0400

    Organized Project folders and created EnemyController and PlayerManager scripts

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Ideas for code came from Brackeys https://www.youtube.com/watch?v=xppompv1DBg&t=1s
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Singleton
+
+    public static PlayerManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject player;
+}
